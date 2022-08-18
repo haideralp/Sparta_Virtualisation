51614323b88c5de2c5c0c582b8296ef2ef77c73f
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-18 09:07:01 +0100

    commit

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,18 @@
 # Ruby 
 
 Vagrant.configure("2") do |config|
-    config.vm.define "app" do |app| # define app
-      app.vm.box = "ubuntu/bionic64" # state version and os
-      app.vm.network "private_network", ip: "192.168.10.100" # configure network
-      app.vm.synced_folder ".", "/home/vagrant/app" # change it to your home location 
-      app.vm.provision "shell", path: "provision.sh", privileged: false  # provide path for your provision.sh 
-    end
-  
     config.vm.define "db" do |db|
       db.vm.box = "ubuntu/bionic64"
       db.vm.network "private_network", ip: "192.168.10.150"
       db.vm.synced_folder ".", "/home/vagrant/app" # sync directoy. 
       db.vm.provision "shell", path: "dbprov.sh", privileged: false #provisin sh path for db
-      
+    end
+  
+    config.vm.define "app" do |app| # define app
+      app.vm.box = "ubuntu/bionic64" # state version and os
+      app.vm.network "private_network", ip: "192.168.10.100" # configure network
+      app.vm.synced_folder ".", "/home/vagrant/app" # change it to your home location 
+      app.vm.provision "shell", path: "provision.sh", privileged: false  # provide path for your provision.sh 
     end
   end
 
--- provision.sh
+++ provision.sh
@@ -8,18 +8,39 @@ sudo apt-get upgrade -y
 # Install & Enable NGINX
 
 sudo apt-get install nginx -y
+sudo systemctl restart nginx
 sudo systemctl enable nginx
-sudo systemctl start nginx
 
 # Install node.js (version 6.0)
-sudo apt-get purge nodejs npm # purges previous versions 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#sudo apt-get purge nodejs npm # purges previous versions 
 sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 # Install npm & pm2
 sudo apt-get install npm
-# sudo npm install pm2  # sudo to override permissions. 
+sudo npm install pm2 -g  # sudo to override permissions. 
 
 # Upgrade and Update Overall 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Automate server Reverse Proxy
+sudo rm /etc/nginx/sites-available/default
+sudo cp -f /Users/haide/Sparta_Virtualisation/app/reverse_prox etc/nginx/sites-available/default
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+# Setting Environment Variable in the enviroment on vm
+echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
+
+# Navigate to cd app/app for npm install 
+cd app/app
+npm install
+
+# Data fetching in seeds folder
+cd seeds
+sudo node seed.js
+
+# Remaining Updates / Upgrades Performed
 sudo apt-get update -y
 sudo apt-get upgrade -y
